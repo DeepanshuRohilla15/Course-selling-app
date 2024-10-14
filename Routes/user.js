56e6c7aee88366ec675c71c3abe2c868948be617
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-14 23:06:07 +0530

    Add routing to index.js

--- Routes/user.js
+++ Routes/user.js
@@ -3,9 +3,7 @@ const {Router} = require("express");
 const userRouter = Router();
 
 userRouter.post("/signup", function(req, res){
-    res.json({
-        message: "Signup Endpoint"
-    })
+    
 })
 
 userRouter.post("/signin", function(req, res){
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const {userRouter}  = require("./Routes/user");
 const {courseRouter} = require("./Routes/course")
-const jwt = require("jsonwebtoken");
-const { courseRouter } = require("./Routes/course");
 const app = express();
 
-app.use("/user", userRouter);
-app.use("/course", courseRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
 
 
 
