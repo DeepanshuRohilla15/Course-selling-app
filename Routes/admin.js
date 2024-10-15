2ea9826399d7ed1e134a2eeb29297a8d0c74fe8a
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-15 12:33:13 +0530

    remove some un-necessay things

--- Routes/admin.js
+++ Routes/admin.js
@@ -12,17 +12,17 @@ adminRouter.post("/signin", function(req, res){
 })
 
 // admin create a course
-adminRouter.post("/course", function(req, res){
+adminRouter.post("/", function(req, res){
 
 })
 
 //change the course, price
-adminRouter.put("/course", function(req, res){
+adminRouter.put("/", function(req, res){
 
 })
 
 // see all the courses created by admin
-adminRouter.get("/course/bulk", function(req, res){
+adminRouter.get("/bulk", function(req, res){
 
 })
 
