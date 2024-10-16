c9eb0700d8a7102b866ea4e12d4c1a43e6fbd601
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-16 18:56:48 +0530

    setup the config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const JWT_USER_PASSWORD = "aldadscn23";
-const JWT_ADMIN_PASSWORD = "starkeffect";
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
 
 module.exports = {
     JWT_ADMIN_PASSWORD,
